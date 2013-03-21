a92ac5e94e51e254cfc49646be94403f3ebce1dc
Dmitroza <dmitroza@gmail.com>
2013-03-21 17:48:18 +0200

    validation added

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,4 @@
 class Property < ActiveRecord::Base
   attr_accessible :approx_sqft, :bathrooms, :bedrooms, :city, :geo_lat, :geo_lon, :list_price, :street_name, :street_number, :street_suffix, :unit_number, :year_built, :zip_code
+	validates :city, :street_name, :street_number, :presence => true
 end
