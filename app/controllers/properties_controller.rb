33202d63420654cec704176d6d45c17490d43d39
Dmitroza <dmitroza@gmail.com>
2013-03-21 18:32:21 +0200

    crud for properties were implemented

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -10,6 +10,7 @@ class PropertiesController < ApplicationController
   def new
   	@property = Property.new
   end
+
   def create 
   	@property = Property.new(params[:property])
 		if @property.save
@@ -18,5 +19,32 @@ class PropertiesController < ApplicationController
 			render :action => "new"
 		end
   end	
+
+  def destroy
+		@property = Property.find(params[:id])
+		@property.destroy
+		respond_to do |format|
+			format.html { redirect_to root_path }
+			format.json { head :no_content }
+		end
+  end
+
+  def edit
+		@property = Property.find(params[:id])
+  end
+  
+	def update
+		@property = Property.find(params[:id])
+		
+		respond_to do |format|
+			if @property.update_attributes(params[:property])
+				format.html { redirect_to(root_path, :notice => 'Property was successfully updated.') }
+				format.json { head :no_content }
+			else
+				format.html { render :action => "edit" }
+				format.json { render :json => @property.errors, :status => :unprocessable_entity }
+			end
+		end
+	end
   
 end
