b68d7e51e0a83304250eb63791c3326532c51ad9
Dmitroza <dmitroza@gmail.com>
2013-03-25 16:06:34 +0200

    in property model user relation was renamed to Owner, this field was shoun in index and show methods

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :owner, :class_name => "User", :foreign_key => "user_id"
   attr_accessible :approx_sqft, :bathrooms, :bedrooms, :city, :geo_lat, :geo_lon, :list_price, :street_name, :street_number, :street_suffix, :unit_number, :year_built, :zip_code
-	validates :city, :street_name, :street_number, :user, :presence => true
+	validates :city, :street_name, :street_number, :owner, :presence => true
 	def full_address
 		[:zip_code, :city, :street_name, :street_suffix, :street_number, :unit_number].map{|m| self.send m }.join(" ")
 		# self.zip_code + " " + self.city + " " + self.street_name + " " + self.street_suffix + " " + self.street_number + " " + self.unit_number
