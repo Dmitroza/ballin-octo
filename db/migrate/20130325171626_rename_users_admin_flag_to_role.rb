592e43fda7e4fc31a604542fa951234431e9d338
Dmitroza <dmitroza@gmail.com>
2013-03-26 14:31:58 +0200

    user name available on top-right

--- db/migrate/20130325171626_rename_users_admin_flag_to_role.rb
+++ db/migrate/20130325171626_rename_users_admin_flag_to_role.rb
@@ -1,13 +1,13 @@
 class RenameUsersAdminFlagToRole < ActiveRecord::Migration
   def up
   	rename_column :users, :admin_flag, :role
-  	change_column :users, :role, :string
+  	change_column :users, :role, :string, :default => 'REGUSER'
   	User.where(:email => "admin@example.com").first.update_attribute :role, 'ADMIN'
   end
 
   def down
   	rename_column :users, :role, :admin_flag
-  	change_column :users, :admin_flag, :boolean
+  	change_column :users, :admin_flag, :boolean, :default => false
   	User.where(:email => "admin@example.com").first.update_attribute :role, true
   end
 end
